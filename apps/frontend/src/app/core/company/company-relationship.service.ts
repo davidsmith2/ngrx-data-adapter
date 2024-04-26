f3f867755548e339d9e3f9f050a9a2a6706c09f1
David Smith <davidsmith2@gmail.com>
2024-04-26 08:52:53 -0400

    fix

--- apps/frontend/src/app/core/company/company-relationship.service.ts
+++ apps/frontend/src/app/core/company/company-relationship.service.ts
@@ -30,7 +30,7 @@ export class CompanyRelationshipService implements DefaultRelationshipService<Co
       'addressId',
       'address',
       {
-        gqlFields: ['id', 'street']
+        gqlFields: ['id', 'street', 'city', 'country']
       }
     ),
     childrenEntities(
